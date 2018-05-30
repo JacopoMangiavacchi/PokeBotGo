844d195a23ccf6d99e5d38908f92002b1803675d
Jacopo <jmangia@me.com>
2018-05-29 18:38:44 -0700

    no message

---
+++ server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	pb "pokemon/pokemon"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+//SearchPokemon(*PokeInput, PokeBot_SearchPokemonServer) error
+// SearchPokemon implements PokeBotServer
+func (s *server) SearchPokemon(input *pb.PokeInput, stream pb.PokeBot_SearchPokemonServer) error {
+	fmt.Println(input.Name)
+	//p := pb.Pokemon{}
+	var p pb.Pokemon
+	p.Name = "Jacopo"
+	stream.Send(&p)
+
+	return nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterPokeBotServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
