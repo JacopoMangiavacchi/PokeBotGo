d8b9d6f49e750ff807385fe04aa5219ceab38d0a
Jacopo <jmangia@me.com>
2018-05-30 14:30:09 -0700

    no message

--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
 
 	pb "github.com/JacopoMangiavacchi/PokeBotGo/pokemon"
 
@@ -15,17 +19,20 @@ const (
 	port = ":50051"
 )
 
-// server is used to implement helloworld.GreeterServer.
+// server is used to implement PokeBotServer.
 type server struct{}
 
-//SearchPokemon(*PokeInput, PokeBot_SearchPokemonServer) error
+// SearchPokemon(*PokeInput, PokeBot_SearchPokemonServer) error
 // SearchPokemon implements PokeBotServer
 func (s *server) SearchPokemon(input *pb.PokeInput, stream pb.PokeBot_SearchPokemonServer) error {
 	fmt.Println(input.Name)
-	//p := pb.Pokemon{}
-	var p pb.Pokemon
-	p.Name = "Jacopo"
-	stream.Send(&p)
+	p, error := getPokemon(input.Name)
+
+	if error == nil {
+		stream.Send(&p)
+	} else {
+
+	}
 
 	return nil
 }
@@ -42,4 +49,50 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+type pokemon struct {
+	Id     int32  `json:"id"`
+	Name   string `json:"name"`
+	Height int32  `json:"height"`
+	Weight int32  `json:"weight"`
+}
+
+func getPokemon(name string) (pb.Pokemon, error) {
+	var p pb.Pokemon
+
+	req, err := http.NewRequest("GET", "https://pokeapi.co/api/v2/pokemon/bulbasaur", nil)
+	if err != nil {
+		return p, errors.New("wrongUrl")
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return p, errors.New("notFound")
+	}
+
+	defer resp.Body.Close()
+
+	body, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+		return p, errors.New("wrongBody")
+	}
+
+	pokemon1 := pokemon{}
+	jsonErr := json.Unmarshal(body, &pokemon1)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+		return p, errors.New("jsonUnmarshal")
+	}
+
+	p.Name = pokemon1.Name
+	p.Id = pokemon1.Id
+	p.Height = pokemon1.Height
+	p.Weight = pokemon1.Weight
+
+	return p, nil
+	//return p, errors.New("notFound")
 }
